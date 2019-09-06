47559cf1c0ab9d775542aed2fe1f0eb9d4709b66
apple <wzw2984521645@163.com>
2019-09-06 22:25:20 +0800

    dubbo项目提交             完成版本

--- dubbo-consumer/src/main/java/com/wzw/controller/TestController.java
+++ dubbo-consumer/src/main/java/com/wzw/controller/TestController.java
@@ -2,7 +2,6 @@ package com.wzw.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.wzw.service.DubboTest;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
--- dubbo-provider/src/main/java/com/wzw/TestdemoApplication.java
+++ dubbo-provider/src/main/java/com/wzw/TestdemoApplication.java
@@ -1,9 +1,9 @@
 package com.wzw;
 
+
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableDubbo
